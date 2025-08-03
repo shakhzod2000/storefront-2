7d21ab8fa9ba29225b418bdc24a292258feab843
sshermat <sshermat@42berlin.de>
2025-08-03 16:51:23 +0500

    initial commit

---
+++ store/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+
+# create tables(models) in DB
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    # ↓ if parent(Collection) class defined before child(Product), use ''
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    # related_name='+' tells Django not to create reverse relationship
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    # slug is uniquely identifying last part of URL
+    slug = models.SlugField()
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)  # auto_now - updates(overwrites) datetime on every Product update
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    # ↓ Django creates reverse relationship btwn Promotion & Product
+    promotions = models.ManyToManyField(Promotion)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['first_name', 'last_name'])
+        ]
+
+
+class Order(models.Model):
+    PENDING_PAYMENT = 'P'
+    COMPLETE_PAYMENT = 'C'
+    FAILED_PAYMENT = 'F'
+
+    PAYMENT_STATUSES = [
+        (PENDING_PAYMENT, 'Pending'),
+        (COMPLETE_PAYMENT, 'Complete'),
+        (FAILED_PAYMENT, 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True) # auto_now_add - adds datetime for each product update(doesn't overwrite)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUSES, default=PENDING_PAYMENT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    # prevent negative values with `PositiveSmallIntegerField()`
+    # max of PositiveSmallIntegerField = 32767, 16-b int (2^15 - 1)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=10, default='')
+    # below is one to many relationship between two models
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
+    # we set `Customer` as a parent class of `Address`
+    # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    # if product deleted, it should be removed from all CartItems
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
