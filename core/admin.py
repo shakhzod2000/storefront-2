d680ff0353d5c733771f9fdb60b843b65643ade7
sshermat <sshermat@42berlin.de>
2025-09-04 17:34:49 +0500

    add core app

---
+++ core/admin.py
@@ -0,0 +1,37 @@
+# core/admin.py
+from store.models import Product
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from store.admin import ProductAdmin
+from tags.models import TaggedItem
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
+
+
+class TagInline(GenericTabularInline):
+    model = TaggedItem
+    autocomplete_fields = ['tag']
+    min_num = 1
+    max_num = 5
+    extra = 0
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
---
+++ core/models.py
@@ -0,0 +1,7 @@
+# core/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
